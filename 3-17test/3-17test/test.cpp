d10d088efb0af3069db9c16fa5ad37da86d9c510
zjpgitte <1595873940@qq.com>
2021-03-17 21:22:35 +0800

    练习

---
+++ 3-17test/3-17test/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char str1[] = "hello";
+	char str2[] = "world";
+	
+	char *const ps[] = {str1, str2};
+	const char* pst = str1;
+	
+
+	
+
+
+	return 0;
+}
