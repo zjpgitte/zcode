73103632dd043555397ccf1212550b347d327826
zjpgitte <1595873940@qq.com>
2021-04-01 11:46:01 +0800

    简单shell程序

---
+++ minishell/myproc.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAXSIZE 1024
+using namespace std;
+
+int main()
+{
+
+    while(1)
+    {
+        pid_t id = fork();
+        if(id < 0)
+        {
+            cout << "fork error!" << endl;
+            exit(0);
+        }
+        else if(id == 0)
+        {
+            //child 
+            //显示命令行界面 [minishell]# 
+            cout << "[minishell]# ";
+
+            //输入命令保存在cmd中
+            char cmd[MAXSIZE] = {'\0'};
+            fgets(cmd, MAXSIZE -1, stdin);
+            cmd[strlen(cmd) - 1] = '\0';   //将读入的\n取消
+
+            //解析string,将指令和参数保存到 args[] 中
+            char *args[MAXSIZE / 100] = {NULL}; 
+            char *ps = strtok(cmd, " ");
+            int i = 0;
+            while(ps)
+            {
+                args[i++] = ps;
+                ps = strtok(NULL, " ");
+            }
+            //进程替换
+            execvp(args[0], args);
+            exit(0);
+        }
+
+        //父进程等待子进程的执行
+        int status = 0;
+        pid_t ret = waitpid(id, &status, 0);
+
+        //检测等待过程是否成功
+        if(ret == -1)
+        {
+            //函数调用失败
+            cout << "wait error!" << endl;
+        }
+        else if(ret == id)
+        {
+            if(WIFEXITED(status))
+            {
+                //等待成功
+                cout << WEXITSTATUS(status) << endl;
+            }
+            else 
+            {
+                cout << "killed by sign!" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
